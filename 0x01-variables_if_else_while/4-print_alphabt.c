dd50a87d808cee6bd13818e13d00aa8a17f0d9f2
AndresSern <andrecamposer@gmail.com>
2020-09-17 23:08:28 +0000

    Word between a and z, except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,14 @@
 int main(void)
 {
 	char ch = 'a';
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-        	if ((ch != 'q') && (ch != 'e')) 
+		if ((ch != 'q') && (ch != 'e'))
 		{
 			putchar(ch);
-        	}
-    	} 
+		}
+	}
 
 	putchar('\n');
 
