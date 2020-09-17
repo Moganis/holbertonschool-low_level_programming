66acbd6ba5f9b46010778bacc03a77df862d42f1
AndresSern <andrecamposer@gmail.com>
2020-09-17 22:21:22 +0000

    Print Alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Imprimir abecadario con letras mayusculas
+* Return: 0 for success
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+	
+}
+}
