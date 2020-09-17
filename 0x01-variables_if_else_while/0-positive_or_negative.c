6372d9bc7b8774a58eaf434b2ee96aec84520999
AndresSern <andrecamposer@gmail.com>
2020-09-17 20:13:23 +0000

    First positive and negative random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,14 @@ int main(void)
 	int n;
 
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	if (n == 0
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
@@ -25,5 +26,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
