b853baaff3eef989a4eb71b486e820de237d486b
AndresSern <andrecamposer@gmail.com>
2020-09-18 00:16:46 +0000

    Print Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,14 +3,14 @@
 #include <time.h>
 
 /**
-* main - Imprimir abecadario con letras mayusculas
+* main - Print number from 0 to 9 without using char
 * Return: 0 for success
 */
 int main(void)
 {
-	char ch = '0';
+	int ch;
 
-	for (ch = '0'; ch <= '9'; ch++)
+	for (ch = 0; ch <= 9; ch++)
 		putchar(ch + '0');
 	putchar('\n');
 	return (0);
