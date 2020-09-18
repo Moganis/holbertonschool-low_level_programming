2c07a7c0b565aac55b4b08be42d87115a046943e
AndresSern <andrecamposer@gmail.com>
2020-09-18 04:40:24 +0000

    Print 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - De 0 a 99 con comas
+* Return: 0 for success
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 100; ch++)
+	{
+	putchar((ch / 10) + '0');
+	putchar((ch % 10) + '0');
+		if (ch != 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	putchar('\n');
+	}
+	return (0);
+}
