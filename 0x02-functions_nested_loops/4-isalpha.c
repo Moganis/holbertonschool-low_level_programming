eb4ea05db94950cb406fc287abbd5cdc8deffea7
AndresSern <andrecamposer@gmail.com>
2020-09-22 05:17:38 +0000

    Deteminate if is lower or upper

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * _isalpha - check the the entry to deteminate if is lower or upper
  *
+ * @c: This is the entry
  * Return: Always 0.
  */
 
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        {
-            return (1);
-        }
-        else
-        {
-            return (0);
-        }
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
