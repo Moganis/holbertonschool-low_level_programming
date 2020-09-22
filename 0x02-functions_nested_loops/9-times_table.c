c2621440ec0b86d9a6ab967ae36a2a1cf3890957
AndresSern <andrecamposer@gmail.com>
2020-09-22 22:34:17 +0000

    Tabla hasta el 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,18 +7,23 @@
  * Return: Always 0.
  */
 
+
 void times_table(void)
-{    
+{
     int a,b,c;
-
+    
     for(a = 0; a <= 9; a++){
-
         for (b = 0; b <= 9; b++)
         {
             c = a * b;
-            if(c <= 9)
+            
+            if (b == 0)
             {
-		_putchar(32);
+                _putchar(c + '0');
+            }
+            else if(c <= 9 )
+            {
+                _putchar(32);
                 _putchar(c + '0');
             }
             else
