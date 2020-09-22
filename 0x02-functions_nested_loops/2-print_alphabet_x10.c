1dcfdc708e3817a51bb7d2c3205739500b880d38
AndresSern <andrecamposer@gmail.com>
2020-09-22 05:08:24 +0000

    Imprime el abecedario 10 veces

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet_x10 - This function print alphabet x10
  *
  * Return: Always 0.
  */
@@ -9,12 +9,13 @@
 void print_alphabet_x10(void)
 {
 	int a, b;
+
 	for (a = 0; a <= 9 ; a++)
-	{	
+	{
 		for (b = 97; b <= 122; b++)
 		{
 			_putchar(b);
-		}	
+		}
 	_putchar('\n');
 	}
 }
