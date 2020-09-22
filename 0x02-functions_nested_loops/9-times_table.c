c889a76b33c8838ee8a0cfc1408ced97f2bda742
AndresSern <andrecamposer@gmail.com>
2020-09-22 22:47:36 +0000

    Tabla hasta el 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,42 +1,42 @@
 #include "holberton.h"
 
 /**
- * _isalpha - check the the entry to deteminate if is lower or upper
+ * times_table - It is a table multiplication
  *
- * @c: This is the entry
+ *
  * Return: Always 0.
  */
 
-
 void times_table(void)
 {
-    int a,b,c;
-    
-    for(a = 0; a <= 9; a++){
-        for (b = 0; b <= 9; b++)
-        {
-            c = a * b;
-            
-            if (b == 0)
-            {
-                _putchar(c + '0');
-            }
-            else if(c <= 9 )
-            {
-                _putchar(32);
-                _putchar(c + '0');
-            }
-            else
-            {
-                 _putchar((c / 10 ) + '0');
-                 _putchar((c % 10 ) + '0');
-            }
-            if(b != 9)
-            {
-                _putchar(',');
-                _putchar(32);
-            }
-        }
-        _putchar('\n');
-    }
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			else if (c <= 9)
+			{
+				_putchar(32);
+				_putchar(c + '0');
+			}
+			else
+			{
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			}
+			if (b != 9)
+			{
+			_putchar(',');
+			_putchar(32);
+			}
+		}
+	_putchar('\n');
+	}
 }
