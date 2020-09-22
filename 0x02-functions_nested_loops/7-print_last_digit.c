ad73cf980701689ffda5a4243cc1e888b24c3888
AndresSern <andrescamposer@gmail.com>
2020-09-22 19:43:00 +0000

    Ultimo numero

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,25 +9,11 @@
  **/
 
 int print_last_digit(int n)
-{
+{n = n % 10;
 
-	if (n > 0)
-        {
-                n = n % 10;
-                _putchar('0' + n);
-		return (n);
-        }
-        else if (n < 0)
-        {
-                n =  -n;
-                n = n % 10;
-                _putchar('0' + n);
-		return(n);
-        }
-        else
-        {
-                _putchar('0' + n);
-		return (n);
-        }
+	if (n < 0)
+		n = -n;
+	_putchar(n + '0');
+	return (n);
         
 }
