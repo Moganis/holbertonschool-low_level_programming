8c4213fdb46eeb97b96983b818186cea0d797c98
AndresSern <andrecamposer@gmail.com>
2020-09-22 22:57:07 +0000

    Addicion de dos numeros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - check the the entry to deteminate if is lower or upper
+ *
+ * @c: This is the entry
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+     int c;
+    c = a + b;
+    return (c);
+}
