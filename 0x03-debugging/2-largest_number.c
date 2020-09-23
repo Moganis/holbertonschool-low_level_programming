53e029a3b44fb713535c34f8eec9527e5f3e22ad
AndresSern <andrecamposer@gmail.com>
2020-09-23 03:31:43 +0000

    Mayor than

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a == b && a > c)
+	{
+	largest = a;
+	}
+	else if (a == c && a > b)
+	{
+	largest = b;
+	}
+	else if (a > b && a > c)
 	{
 	largest = a;
 	}
