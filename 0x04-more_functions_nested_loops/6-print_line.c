57c2b31b11ee779e1d913695410febfb104f8f53
AndresSern <andrecamposer@gmail.com>
2020-09-24 18:42:52 +0000

    print line with your number

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n >= 1 )
+	{
+		for (a = 1; a <= n; a++)
+		{
+			_putchar(95);
+		}
+	
+	}
+	_putchar('\n');
+}
