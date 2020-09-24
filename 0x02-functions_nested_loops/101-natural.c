55eb6d597fa03249febff63c28de0500edf159bf
AndresSern <andrecamposer@gmail.com>
2020-09-24 04:15:27 +0000

    Natural advanced

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,9 +5,9 @@
  *
  *
  *
- * 
- *
  */
+
+
 int numbers(void)
 {
  int b;
@@ -15,15 +15,22 @@ int numbers(void)
     {
         if( a % 3 == 0 )
         {
-            printf("%d, ", a);
+            printf("%d", a);
             b = a + b;
+            if (a != 1023)
+        {
+            printf(", ");
+        }
         }
         else if (a % 5 == 0)
         {
             b = a + b;
-            printf("%d, ", a);
+            printf("%d", a);
+            if (a != 1023)
+        {
+            printf(", ");
         }
-
+        }
     }
      printf("\n");
      printf("%d", b);
