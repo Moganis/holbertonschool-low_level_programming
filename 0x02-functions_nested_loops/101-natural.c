a566b3135409d86d1ef7dced6a8b82645309c829
AndresSern <andrescamposer@gmail.com>
2020-09-24 04:00:11 +0000

    advanced 2

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ * 
+ *
+ */
+
+void numbers(void)
+{
+ int b;
+    for(int a = 1; a < 1024; a++)
+    {
+        if( a % 3 == 0 )
+        {
+            printf("%d, ", a);
+            b = a + b;
+        }
+        else if (a % 5 == 0)
+        {
+            b = a + b;
+            printf("%d, ", a);
+        }
+
+    }
+     printf("\n");
+    printf("the result is %d", b);
+}
