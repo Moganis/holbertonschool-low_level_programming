0693d07b2824a4517bdf0f8005130389aacefe0b
AndresSern <andrecamposer@gmail.com>
2020-09-24 04:27:32 +0000

    Natural advanced

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 /**
+ * main - this multiple 5 and 3 of 1024
  *
  *
- *
- *
+ * Return: 0
  */
 
 
 int main(void)
 {
- int b , a;
-    for( a = 1; a < 1024; a++)
-    {
-        if( a % 3 == 0 )
-        {
-            b = a + b;
-        }
-        else if (a % 5 == 0)
-        {
-            b = a + b;
-        }
-    }
-     printf("%d\n", b);
-     return (0);
+	int b, a;
+
+	for (a = 1; a < 1024; a++)
+	{
+	if (a % 3 == 0)
+	{
+		b = a + b;
+	}
+	else if (a % 5 == 0)
+	{
+		b = a + b;
+	}
+	}
+	printf("%d\n", b);
+	return (0);
 }
