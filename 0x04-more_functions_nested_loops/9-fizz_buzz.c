a89908e399e3d46c4029d8c62e6c9fce413ba858
AndresSern <andrecamposer@gmail.com>
2020-09-25 02:04:43 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,13 +16,13 @@ int main (void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (a % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if(a % 3 == 0)
+		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
+		}
+		else if(a % 5 == 0)
+		{
+			printf("Buzz ");
 		}
 		else
 		{
