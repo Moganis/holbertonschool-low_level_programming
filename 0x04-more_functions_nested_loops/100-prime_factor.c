464eb3e8e789d1af1bfd16d2dc01c5bb509eeaf2
AndresSern <andrecamposer@gmail.com>
2020-09-26 04:02:43 +0000

    the proyect 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/**
+ * main - Prints the prime number of
+ *
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int newValue, i;
 
+	long int number = 612852475143;
 
-
-int main(void){
-
-	int newValue;
-        long int number = 612852475143;
-        int i;
-
-        for (i = 2; i <=
-         number ; i++)
-        {
-            if(number % i == 0 )
-            {
-                number  = number / i;
-		newValue = i;
-            }
-        }
-        printf("%d\n", newValue);
-
-        return 0;
-
+	for (i = 2; i <= number; i)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+			newValue = i;
+		}
+	}
+	printf("%d\n", newValue);
+	return (0);
 }
