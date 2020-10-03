a8afd901dc6205abadb7bdb3a79c3836687536c9
AndresSern <andrecamposer@gmail.com>
2020-10-03 03:45:58 +0000

    This is the second

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a, b, c;
+    
+    for(a = 0; a < n && src[a] != '\0'; a++)
+    {
+        dest[a] = src[a];
+    }
+    if (a < n)
+    {
+        dest[a] = '\0';
+    }
+    return dest;
+}
