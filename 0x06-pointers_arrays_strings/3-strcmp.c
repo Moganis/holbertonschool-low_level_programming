9c5b090ee13451b984e4d3373d9884280d9debf0
AndresSern <andrecamposer@gmail.com>
2020-10-04 07:13:32 +0000

    Exercise Number 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
+/**
+ * _strcmp - This is my function
+ * @s1: This is my entry
+ * @s2: This is my entry again
+ * Return: This is my result
+ */
 
-int _strcmp(char *strg1, char *strg2)
+int _strcmp(char *s1, char *s2)
 {
-    for(; (*strg1 != '\0' && *strg2 != '\0') && *strg1 == *strg2; strg1++, strg2++)
-    {
-    }
-    
-        if(*strg1 == *strg2)
-    {
-        return 0;
-	}	
-
-    else
-    {
-        return *strg1 - *strg2;
-    }
-    
+	for (; (*s1 != '\0' && *s2 != '\0') && *s1 == *s2; s1++, s2++)
+	{
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
