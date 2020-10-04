464d546f6a1fb154904dc7ce6bb1a7c2b43e0043
AndresSern <andrecamposer@gmail.com>
2020-10-04 05:15:38 +0000

    Exercise 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,4 +1,12 @@
 #include "holberton.h"
+/**
+ * rot13 - This is my funcion
+ * @hi: This is my entry
+ *
+ *
+ * Return: This is my result
+ *
+ */
 
 char *rot13(char *hi)
 {
@@ -9,15 +17,15 @@ char *rot13(char *hi)
 
 	for (i = 0; hi[i] != '\0'; ++i)
 	{
-		for(a = 0; minus[a] != '\0' ; a++)
+		for (a = 0; minus[a] != '\0' ; a++)
 		{
-			if(hi[i] == minus[a])
+			if (hi[i] == minus[a])
 			{
 				hi[i] = mayus[a];
-			    break;
+				break;
 			}
 		}
 	}
 	hi[i] = '\0';
-	return hi;
+	return (hi);
 }
