72414be2e1fa9a918217e2ac734a7da784e6cfff
AndresSern <andrecamposer@gmail.com>
2020-10-05 19:05:55 +0000

    Exercise 3

--- 0x07-pointers_arrays_strings/3-main.c
+++ 0x07-pointers_arrays_strings/3-main.c
@@ -10,9 +10,13 @@ int main(void)
 {
     char *s = "hello, world";
     char *f = "oleh";
-    unsigned int n;
-
+    unsigned int n, i;
+	char *strtext = "129th";
+        char *cset = "1234567890";
     n = _strspn(s, f);
     printf("%u\n", n);
+
+	 i = _strspn (strtext,cset);
+	printf("%u",i);
     return (0);
 }
--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,7 +10,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int a, b, c;
-
+	c = 0 ;
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		for (b = 0; accept[b] != '\0'; b++)
