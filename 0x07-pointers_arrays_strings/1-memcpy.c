0fa7a626b231f58a36d499e1b8632c35ad810187
AndresSern <andrecamposer@gmail.com>
2020-10-05 04:33:16 +0000

    Exercise one

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - This is my funcion copy Src to dest
+ * @dest: This is my result
+ * @src: This is my string
+ * @n: This is n my number bytes to copy
+ *
+ * Return: This my my string copy
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		*dest = src[a];
+		dest++;
+	}
+	return (dest);
+}
