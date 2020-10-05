d3493713b830f071cd9034e2f37c16b4b96a9d0b
AndresSern <andrecamposer@gmail.com>
2020-10-05 18:26:51 +0000

    Exercise 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 	char *pDst = (char *) dest;
-	char const *pSrc =(char *) src;
+	char const *pSrc = (char *) src;
 
 	for (a = 0; a < n; a++)
 	{
