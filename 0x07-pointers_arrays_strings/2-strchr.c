ba0dd99c581900c845a4fb68fa664352d0967778
AndresSern <andrecamposer@gmail.com>
2020-10-06 04:06:44 +0000

    Exercise 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,12 @@
 #include "holberton.h"
+#include <stddef.h>
 
 char *_strchr(char *s, char c)
-{
-        unsigned int a;
-	char *result;
-	char *changed;
-	*changed = c;
-        c = 0;
-        for (a = 0; s[a] != '\0'; a++)
-        {
-		if(s[a] == *changed)
-		{
-		*result = s[a];
-		break;
-		}
-		else
-		{
-		*result = NULL;
-		}
-        }
-	return result;
+{   
+	for(; ; s++)
+	{
+		if(*s == c)
+			return (s);
+	}
+	return NULL;
 }
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,5 +7,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
 void print_chessboard(char (*a)[8]);
+char *_strchr(char *s, char c);
 
 #endif
